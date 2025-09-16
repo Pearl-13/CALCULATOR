cfa948c5e878e7f74e01c473b49d1e65b290570b
Pearl-13 <pearlwebco13@gmail.com>
2025-09-16 01:40:50 +0100

    Add files via upload

---
+++ CALCULATOR.js
@@ -0,0 +1,63 @@
+console.log("my code is running");
+let currentInput = "";
+let previousInput = "";
+let operator = null;
+const operatorbtn =
+document.querySelectorAll(".operator");
+let numberbtn= 
+document.querySelectorAll(".number");
+const equalToSign =
+document.getElementById("equals");
+let result =
+document.getElementById("result");
+const percent =
+document.getElementById("percent");
+numberbtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        result.value += btn.textContent
+    })
+    
+});
+
+operatorbtn.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", () => {
+        result.value += operatorButton.textContent
+    })
+});
+const pi = () =>{
+    document.getElementById(value).textContent = Math.PI(Number(eval(result.textContent)));
+}
+function insert(value) {
+    if (value === "CLEAR") {
+        result.value = "";
+    }  
+    else if (value === "DELETE") {
+        result.value = 
+        result.value.slice(0,-1);
+    }
+    else if (value === "=") {
+        try {
+            result.value =
+            eval(result.value);
+        } catch {
+            result.value = "SYNTAX ERROR"
+        }
+    }
+   
+
+}
+function evaluateSign() {
+    let input = result.value;
+    input = 
+    input.replace(/%/g, "/100");
+    input =
+    input.replace(/×/g, "*");
+    input = 
+    input.replace(/⊼/g, "3.142");
+    try {
+        let result = eval(input);
+        result.value = result;
+    } catch (e) {
+        result.value = "SYNTAX ERROR";
+    }
+}
